cacc056e2f437f6d5a62808c6d0536acdd10dfa4
Ubuntu <ubuntu@ip-172-31-40-65.ap-northeast-1.compute.internal>
2023-08-17 07:24:52 +0000

    Use SSL and the Puma web server in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,10 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = false
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
