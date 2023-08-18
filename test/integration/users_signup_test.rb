d5589f2b1c141c884998008b764f85905b784de8
Ubuntu <ubuntu@ip-172-31-40-65.ap-northeast-1.compute.internal>
2023-08-18 06:43:22 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -23,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
   
 end
