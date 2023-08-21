8f5e06e27edd3e41800dd762fa09fa402f1a1239
Ubuntu <ubuntu@ip-172-31-40-65.ap-northeast-1.compute.internal>
2023-08-21 02:33:58 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,21 +4,44 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-  
-  def current_user
-      if session[:user_id]
-        User.find_by(id: session[:user_id])
-      end
+
+  # 永続的セッションのためにユーザーをデータベースに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
-    # ユーザーがログインしていればtrue、その他ならfalseを返す
+
+  # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+       # テストがパスすれば、この部分がテストされていないことがわかる
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
 
+
+ # 永続的セッションを破棄する
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
   # 現在のユーザーをログアウトする
   def log_out
+    forget(current_user)
     reset_session
-    @current_user = nil   # 安全のため
+    @current_user = nil
   end
-  
 end
